ed6219ecd49139e51b6a75a9a7d10052c2bde6e1
jhu97 <68985722+jhu97@users.noreply.github.com>
2020-11-03 15:46:04 +0900

    Create 2010.py

---
+++ 2010.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+N = int(input())
+codes = []
+numbers = 0
+for _ in range(N):
+    code = int(input())
+    codes.append(code)
+for i in range(N - 1):
+    numbers += codes[i] - 1
+numbers += codes[-1]
+print(numbers)
