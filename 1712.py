6941bf893c113fd276749e5ad2c7eebe6ac75c10
jhu97 <68985722+jhu97@users.noreply.github.com>
2021-07-09 09:12:04 +0900

    Update 1712.py

--- 1712.py
+++ 1712.py
@@ -16,3 +16,21 @@ if C > B:
         print(-1)
 else:
     print(-1)
+
+'''
+import sys
+import math
+input = sys.stdin.readline
+
+A, B, C = map(int, input().split())
+
+if B != C:
+    n = math.floor(A / (C - B)) + 1
+else:
+    n = -1
+
+if n > 0:
+    print(n)
+else:
+    print(-1)
+'''
