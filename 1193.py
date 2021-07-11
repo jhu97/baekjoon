4daf4c0f14d3df1be79c095e56bb4aba232fe03d
jhu97 <68985722+jhu97@users.noreply.github.com>
2021-07-11 15:36:05 +0900

    Update 1193.py

--- 1193.py
+++ 1193.py
@@ -12,3 +12,20 @@ if n % 2 == 0:
 else:
     answer = '{0}/{1}'.format(int(1 + x - N), int(N + n - x))
 print(answer)
+
+'''
+import sys
+import math
+input = sys.stdin.readline
+
+X = int(input())
+
+n = math.ceil((-1 + math.sqrt(1 + 8 * X)) / 2)
+rest = X - n * (n - 1) / 2
+N = n + 1
+
+if N % 2 == 0:
+    print('{}/{}'.format(int(N - rest), int(rest)))
+else:
+    print('{}/{}'.format(int(rest), int(N - rest)))
+'''
