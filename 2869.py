4a6c126f5dd50fab23a006d4275a01eb28365219
jhu97 <68985722+jhu97@users.noreply.github.com>
2021-07-12 08:57:10 +0900

    Update 2869.py

--- 2869.py
+++ 2869.py
@@ -8,3 +8,15 @@ if N == math.floor(N):
     print(int(N))
 else:
     print(int(math.floor(N) + 1))
+
+'''
+import sys
+import math
+input = sys.stdin.readline
+
+A, B, V = map(int, input().split())
+
+day = math.ceil((V - B) / (A - B))
+
+print(day)
+'''
